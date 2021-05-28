2f0838d412728c040e1415d99a9a0aedceceecf0
Aidan_ogg <49934724+Aidan4444@users.noreply.github.com>
2021-05-28 00:48:24 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,8 @@ fx_version "cerulean"
 game "gta5"
 
 author "Aidan_ogg#0001"
-description "Requires [mumble-voip && rp-radio]"
+description "Requires [mumble-voip/pma-voice && rp-radio]"
+version '1.031'
 
 client_scripts {
     'config.lua',
